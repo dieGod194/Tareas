1c7149dec8fa1f4e882c0fe7f6c123f22b4ff0ca
dieGod194 <122294998+dieGod194@users.noreply.github.com>
2023-03-15 10:07:31 -0600

    Add files via upload

--- mainPersonas.java
+++ mainPersonas.java
@@ -1,11 +1,30 @@
 package ejercicioPersonas;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class mainPersonas {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingresa tu nombre de usuario:");
+        String usuarioinput = scan.nextLine();
+        String rutausuario = "C:\\Users\\Jugge\\Documents\\Eclipse\\UsuariosJuego\\" + usuarioinput + ".txt";
+        File usuario = new File(rutausuario);
+        if (usuario.exists()) {
+            System.out.println("Bienvenido de vuelta " + usuarioinput);
+        } else {
+            try {
+                usuario.createNewFile();
+                System.out.println("Bienvenido! " + usuarioinput);
+            } catch (IOException e) {
+                System.out.println("Error al crear archivo de usuario");
+            }
+        }
+
         Persona P1 = new PersonaRandom("Diego");
         Persona P2 = new PersonaRandom("Sara");
         Persona P3 = new PersonaRandom("Andrea");
@@ -20,8 +39,6 @@ public class mainPersonas {
         ArrayList<Persona> listaPersonas = new ArrayList<>(Arrays.asList(P1, P2, P3, P4, P5, P6, P7, P8, P9, P10));
 
         System.out.println("Reglas: Tienes 3 preguntas disponibles, en donde solo podras ingresar el numero del atributo que quieres revisar si es Verdadero (recuerda que solo hay 4), despues de cada pregunta se te proporcionara la lista de las personas que cumplen tus condiciones de busqueda");
-
-        Scanner scan = new Scanner(System.in);
 
         int preguntasRestantes = 3;
 
@@ -44,16 +61,28 @@ public class mainPersonas {
         
 
         System.out.println("Toca adivinar! Lista de personas que cumplen las caracteristicas:");
-        System.out.println(listaPersonas);
-
+        for (Persona persona : listaPersonas) {
+            System.out.println(persona.getNombre());
+        }
         String respuestaFinal = scan.nextLine();
         int numeroAdivinado = Integer.parseInt(respuestaFinal);
 
-        if (numeroAdivinado <= listaPersonas.size() && numeroAdivinado > 0) {
-            String nombreAdivinado = listaPersonas.get(numeroAdivinado - 1).getNombre();
-            System.out.println("Felicidades, adivinaste que la persona es " + nombreAdivinado);
-        } else {
-            System.out.println("Sigue participando, no has adivinado2 correctamente.");
+            if (numeroAdivinado <= listaPersonas.size() && numeroAdivinado > 0) {
+                String nombreAdivinado = listaPersonas.get(numeroAdivinado - 1).getNombre();
+                System.out.println("Felicidades, adivinaste que la persona es " + nombreAdivinado);
+                try {
+                    FileWriter myWriter = new FileWriter(usuario);
+                    myWriter.write("Estadísticas: " + usuarioinput);
+                    myWriter.write("Adivinanzas exitosa: " + nombreAdivinado);
+                    myWriter.close();
+                    System.out.println("Estadísticas guardadas exitosamente.");
+                } catch (IOException e) {
+                    System.out.println("Ocurrió un error al guardar las estadísticas.");
+                    e.printStackTrace();
+                }
+
+            } else {
+                System.out.println("Sigue participando, no has adivinado correctamente.");
+            }
         }
     }
-}
